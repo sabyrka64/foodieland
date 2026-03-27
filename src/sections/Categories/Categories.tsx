b9d445b31f5f47398e11bf351ac3414f7c570c17
Sabyrka <sabyrka64@gmail.com>
2026-03-27 23:29:31 +0500

    feat: enhance Categories component by adding Grid and CategoryCard for dynamic category display

--- src/sections/Categories/Categories.tsx
+++ src/sections/Categories/Categories.tsx
@@ -1,12 +1,17 @@
 import './Categories.scss'
 import Section from '@/layouts/Section'
+import Grid from '@/components/Grid'
+import CategoryCard from '@/components/CategoryCard'
+import { categories } from './categories.data'
 
 export default () => {
   return (
-    <Section
-      title="Categories"
-      titleId="categories"
-      link="View All Categories"
-    ></Section>
+    <Section title="Categories" titleId="categories" link="View All Categories">
+      <Grid columns={6}>
+        {categories.map((category, index) => (
+          <CategoryCard {...category} key={index} />
+        ))}
+      </Grid>
+    </Section>
   )
 }
