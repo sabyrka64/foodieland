2835de90d1f0a7b7c830118c801485239ef645cd
Sabyrka <sabyrka64@gmail.com>
2026-03-27 23:04:31 +0500

    feat: add link prop to Section component for enhanced interactivity

--- src/layouts/Section/Section.tsx
+++ src/layouts/Section/Section.tsx
@@ -1,9 +1,10 @@
 import './Section.scss'
 import type { TSectionProps } from './types'
 import clsx from 'clsx'
+import Button from '@/components/Button'
 
 export default (props: TSectionProps) => {
-  const { className, title, titleId, description, children } = props
+  const { className, title, titleId, description, link, children } = props
 
   return (
     <section
@@ -11,13 +12,18 @@ export default (props: TSectionProps) => {
       aria-labelledby={titleId}
     >
       <header className="section__header">
-        <h2 className="section__title h3" id={titleId}>
+        <h2 className="section__title" id={titleId}>
           {title}
         </h2>
         {description && (
           <div className="section__description">
             <p>{description}</p>
           </div>
+        )}
+        {link && (
+          <Button className="section__link" mode="cyan" href="/">
+            {link}
+          </Button>
         )}
       </header>
       <div className="section__body">{children}</div>
